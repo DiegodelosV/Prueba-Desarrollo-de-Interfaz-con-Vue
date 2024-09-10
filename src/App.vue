c306ae2d57dff941026c24855de952cce07bf5d4
DiegodelosV <diegodelosv@gmail.com>
2024-09-10 14:04:54 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app" class="container">
+    <header class="text-center">
+      <img
+        src="./assets/img/pokemon-logo.png"
+        class="img-fluid my-3"
+        alt="Pokemon Logo"
+        id="logo"
+      />
+      <h1 class="fw-bold">¿Quién es ese Pokémon?</h1>
+      <p class="text-muted">
+        Pokémones descubiertos: <span class="TextoDescubiertos">{{ cantidadDescubiertos }}</span>
+      </p>
+    </header>
+
+    <div class="row">
+      <div class="col-3" v-for="(pokemon, i) in pokemones" :key="i">
+        <PokemonCard
+          :nombre="pokemon.nombre"
+          :imagen="pokemon.imagen"
+          @pokemon-descubierto="marcarComoDescubierto(i)"
+          :es-descubierto="pokemon.descubierto"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import PokemonCard from "./components/PokemonCard.vue";
+
+export default {
+  components: {
+    PokemonCard,
+  },
+  data() {
+    return {
+      pokemones: [], // Array para almacenar los datos de Pokémon
+    };
+  },
+  async mounted() {
+    await this.obtenerPokemones(); // Llamar a la función para obtener los datos y actualizar el estado
+  },
+  methods: {
+    obtenerPokemones() {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon?limit=20")
+        .then((response) => {
+          const resultados = response.data.results;
+          resultados.forEach((pokemon) => {
+            axios.get(pokemon.url).then((resultado) => {
+              this.pokemones.push({
+                nombre: pokemon.name,
+                imagen: resultado.data.sprites.other.dream_world.front_default,
+                descubierto: false,
+              });
+            });
+          });
+        })
+        .catch((error) => console.error(error, "Error al obtener los datos"));
+    },
+    marcarComoDescubierto(i) {
+      this.pokemones[i].descubierto = true;
+    },
+  },
+  computed: {
+    cantidadDescubiertos() {
+      // para contar los pokemones descubiertos
+      return this.pokemones.filter((pokemon) => pokemon.descubierto).length;
+    },
+  },
+};
+</script>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="BoxPokemon card text-center">
+    <img
+      :src="imagen"
+      class="card-img-top"
+      :class="{ desenfocar: !revelado }"
+      alt="pokemon"
+    />
+    <div class="card-body">
+      <div v-if="!revelado">
+        <input
+          v-model="entradaPokemon"
+          @keyup.enter="descubrirPokemon"
+          class="form-control"
+          placeholder="Nombre del Pokémon"
+        />
+        <button @click="descubrirPokemon" class="btn btn-success mt-2">
+          Descubrir
+        </button>
+      </div>
+      <p class="fw-bold fs-5" v-else>{{ nombre }}</p>
+    </div>
+
+    <!-- Modal de alerta de error -->
+    <div
+      class="modal fade"
+      id="modalError"
+      tabindex="-1"
+      aria-labelledby="modalErrorLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="modalErrorLabel">Fallaste!</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            Nombre de Pokemón incorrecto, intenta nuevamente.
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Cerrar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokemonCard",
+  props: {
+    nombre: String,
+    imagen: String,
+  },
+  data() {
+    return {
+      entradaPokemon: "",
+      revelado: false,
+    };
+  },
+  methods: {
+    descubrirPokemon() {
+      if (this.entradaPokemon.toLowerCase() === this.nombre.toLowerCase()) {
+        this.revelado = true;
+        this.$emit("pokemon-descubierto");
+      } else {
+        // Mostrar el modal de error
+        const modalError = new bootstrap.Modal(
+          document.getElementById("modalError")
+        );
+        modalError.show();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.BoxPokemon {
+  margin: 20px;
+  text-align: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.desenfocar {
+  filter: blur(5px) grayscale(100%);
+}
+
+img {
+  margin: 10px auto 0;
+  width: 150px;
+  height: 150px;
+  padding: 10px;
+}
+
+</style>
